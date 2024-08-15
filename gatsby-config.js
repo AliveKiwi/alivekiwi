bf3ba958786f709c9648b9155ee546660121c7b3
AliveKiwi <prateekjais.dev@gmail.com>
2024-08-15 23:35:46 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `alivekiwi.com`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}

0865d022210b06f3d4673fe29ad48d15b562d14e
AliveKiwi <prateekjais.dev@gmail.com>
2024-10-02 18:58:36 +0530

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,24 +6,46 @@ import Seo from '../components/seo';
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
+      {/* <ul>
         {data.allFile.nodes.map((node) => (
           <li key={node.name}>{node.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
       }
     }
   }
 `;
+
+// export const query = graphql`
+//   query {
+//     allFile {
+//       nodes {
+//         name
+//       }
+//     }
+//   }
+// `;
 export const Head = () => <Seo title="My Blog Posts" />;
 
 export default BlogPage;
